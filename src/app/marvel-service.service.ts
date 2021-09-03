f2574d2965177135c297b3b06efcb3b6eb384ed8
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-09-03 19:27:53 +0300

    Styled the project

--- src/app/marvel-service.service.ts
+++ src/app/marvel-service.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarvelServiceService {
 
-  private generalKey = 'https://gateway.marvel.com:443/v1/public/characters?ts=thesoer&apikey=001ac6c73378bbfff488a36141458af2&hash=72e5ed53d1398abb831c3ceec263f18b'
+  private generalKey = 'https://gateway.marvel.com:443/v1/public/characters?ts=thesoer&apikey=001ac6c73378bbfff488a36141458af2&hash=72e5ed53d1398abb831c3ceec263f18b&limit=100'
 
   constructor(private http: HttpClient) { }
 
