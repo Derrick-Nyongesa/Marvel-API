a15d0a8c9977d87ee3db6ddf2136336a5b1aa82d
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-09-03 18:25:00 +0300

    Created navbar and installed bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CharactersComponent } from './characters/characters.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CharactersComponent
+    CharactersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MarvelServiceService } from '../marvel-service.service';
 
 @Component({
   selector: 'app-characters',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersComponent implements OnInit {
 
-  constructor() { }
+  characters : any;
+
+  constructor(private marvelService: MarvelServiceService) { }
 
   ngOnInit(): void {
+    this.marvelService.getCharacters().subscribe(data => {this.characters = {...data}
+      this.characters = [...this.characters.data.results]
+      console.log(this.characters)
+    })
   }
 
 }
